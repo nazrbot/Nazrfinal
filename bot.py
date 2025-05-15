889d22c91e4a6730ba49bd077eeb3801858ae04a
nazrbot <lr461929@gmail.com>
2025-05-15 17:43:12 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -66,4 +66,7 @@ def send_message(chat_id, text, parse_mode=None):
     requests.post(API_URL, json=payload)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port, debug=True)
